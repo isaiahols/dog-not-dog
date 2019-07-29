37bd83aeb388f1cf1d08d35a70a3aae3200b0cbf
isaiahols <isaiahols@gmail.com>
2019-07-29 09:42:31 -0600

    start camera roll access

--- src/library/components/CameraRoll.js
+++ src/library/components/CameraRoll.js
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, CameraRoll } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import CameraRoll from "@react-native-community/cameraroll";
+// import ImgToBase64 from 'react-native-image-base64';
 
-const CameraRoll = () => {
+const CameraRollContainer = (props) => {
+    const [cameraRollImages, loadImages] = useState([])
+
+
     useEffect(() => {
+        console.log(cameraRollImages)
         const params = {
-            assetType: 'Photos'
+            first: 20,
+            assetType: 'Photos',
         }
         CameraRoll.getPhotos(params)
             .then((res) => {
-                console.log(res)
+                console.log(res.edges)
+                loadImages(res.edges)
             })
             .catch(err => {
                 console.warn(err)
@@ -16,12 +24,31 @@ const CameraRoll = () => {
 
     }, [])
 
+    // const convertToBase64 = (imageUrl) => {
+    //     ImgToBase64.getBase64String(imageUrl)
+    //         .then(base64String => doSomethingWith(base64String))
+    //         .catch(err => doSomethingWith(err));
+    // }
+    console.log(cameraRollImages)
+    const displayCameraRoll = cameraRollImages.map((image, i) => {
+        // const base64Image = convertToBase64(image.node.uri)
+        console.log(image.node.image.uri)
+        return (
+            <TouchableOpacity
+                key={i}
+            // onPress={props.updatePhoto(base64Image)} 
+            >
+                <Image source={{ uri: image.node.image.uri }} />
+            </TouchableOpacity>
+        )
+    })
 
     return (
         <View>
             <Text>This is the Camera Roll</Text>
+            {cameraRollImages && displayCameraRoll}
         </View>
     )
 }
 
-export default CameraRoll
+export default CameraRollContainer
